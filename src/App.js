ab195a76b69c253e18aa55976013adb3bc847381
fbcestia <foititikosbasketball@hotmail.com>
2025-03-26 13:41:36 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>🏀 FastBreak Analysis 🏀</h1>
+      <p>Η καλύτερη πλατφόρμα για basketball analytics!</p>
+    </div>
+  );
+}
+
+export default App;
